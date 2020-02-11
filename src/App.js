8dc66d4c41c094a7530d35ce85cdc68f8dc74cc3
tamirissg <tamiris@icamiabas.com.br>
2020-02-11 12:31:00 -0300

    Header criado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo-white.png";
-import "./App.css";
+import "./components/header/style.css";
+import Header from './components/header/index'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1 className="App-title">Front-End Challenge</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <p className="App-intro">Start the test here!</p>
-      </div>
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
     );
   }
-}
-
-export default App;
+  
+  export default App;
