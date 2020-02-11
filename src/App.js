14ea9096095aabd92b8ce172c7150b0d545e51a4
lefsampaio <leticia.cosmetic2011@gmail.com>
2020-02-11 19:26:27 -0300

    agendando eventos

--- src/App.js
+++ src/App.js
@@ -5,36 +5,33 @@ import Header from './components/header/index'
 import { professionalMocks, servicesMocks, clientsMocks } from "./mocks/mocks";
 import MyCalendar from "./components/calendar";
 
-// import Card from "./components/card";
-
-
 export default function App() {
+ 
+  // const [services, setServices] = useState([]);
+  // const [professional, setProfessional] = useState([]);
+  // const [client, setClient] = useState([]);
 
-  const [services, setServices] = useState([]);
-  const [professional, setProfessional] = useState([]);
-  const [client, setClient] = useState([]);
 
+  // useEffect(() => {
+  //   const getServices = servicesMocks.map((doc) => ({
+  //     _id: doc._id,
+  //     ...doc
+  //   }))
+  //   setServices(getServices);
 
-  useEffect(() => {
-    const getServices = servicesMocks.map((doc) => ({
-      _id: doc._id,
-      ...doc
-    }))
-    setServices(getServices);
+  //   const getProfessionals = professionalMocks.map((doc) => ({
+  //     _id: doc._id,
+  //     ...doc
+  //   }))
+  //   setProfessional(getProfessionals);
 
-    const getProfessionals = professionalMocks.map((doc) => ({
-      _id: doc._id,
-      ...doc
-    }))
-    setProfessional(getProfessionals);
+  //   const getClient = clientsMocks.map((doc) => ({
+  //     _id: doc._id,
+  //     ...doc
+  //   }))
+  //   setClient(getClient);
 
-    const getClient = clientsMocks.map((doc) => ({
-      _id: doc._id,
-      ...doc
-    }))
-    setClient(getClient);
-
-  }, []);
+  // }, []);
 
   return (
     <div className="App">
