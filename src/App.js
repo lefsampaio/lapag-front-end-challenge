52a9db428ce9707f31e54fb30444f9e270f60f99
lefsampaio <leticia.cosmetic2011@gmail.com>
2020-02-12 12:02:24 -0300

    modal open

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@
 import React, { useState, useEffect } from "react";
 import "./components/header/style.css";
 import Header from './components/header/index'
-// import Card from './components/card/index'
-// import Button from './components/button/index'
 import { professionalMocks, servicesMocks, clientsMocks } from "./mocks/mocks";
 import MyCalendar from "./components/calendar";
 import Dropdown from './components/dropdown/index'
-import Modal from './components/modal/index'
-// import Card from "./components/card";
+
 
 
 export default function App() {
@@ -52,14 +49,13 @@ export default function App() {
       return  console.log(arr)
      })
 
-    })
+    }
+    )
   }
   pegarcpf();
   return (
     <div className="App">
       <Header />
-      <div className="App-header">
-      </div>
       <MyCalendar />
 
       {professional.map(i => <span key={i._id}> {i.nickname} <MyCalendar /></span>)}
